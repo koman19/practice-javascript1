11e3c9c087d0690305576374ba08e737ed36657f
koman19 <sonodario7700@gmail.com>
2024-09-19 05:23:44 +0000

    9月１６日

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector("h1").textContent="komann"
+
+//documentとは
+/*htmlファイルで書かれているコードを一つのオブジェクトとみなす。*/
+
+//DOM(ドム)
+/*ドキュメントオブジェクト(document object model)の略で
+webページの要素やコンテンツなどをツリー構造で表現したデータモデル*/
+//上記のコードの意味document.querySelector("h1").textContent="自分の名前"
+
+//ドキュメントのなかの("h1")タグを探してね
+//document.querySelector("h1")
