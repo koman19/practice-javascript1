7ee56f9fd73140a28d57689daeb721902755ac1c
koman19 <sonodario7700@gmail.com>
2024-10-04 06:02:58 +0000

    10-4写経

---
+++ 10-4/main copy.js
@@ -0,0 +1,34 @@
+function errorInput(){
+
+
+
+
+
+
+const month = document.getElementById("month").value
+
+const day = document.getElementById("day").value
+
+if(month<1 || month>12){
+alert("適切ちゃう")
+
+return
+
+}
+
+
+
+console.log("month:",month,"day:",day)
+
+
+
+
+
+
+
+
+
+
+}
+
+
